3b08a23eac8257aa66facf3770c4e482211e3b45
Scott Warchal <scott.warchal@gmail.com>
2021-01-13 13:18:57 +0000

    add additional positive control wells

--- plaque_assay/consts.py
+++ plaque_assay/consts.py
@@ -2,6 +2,6 @@ VIRUS_ONLY_WELLS = ("A12", "B12", "C12")
 
 NO_VIRUS_WELLS = ("F12", "G12", "H12")
 
-POSITIVE_CONTROL_WELLS = ("D12", "E12")
+POSITIVE_CONTROL_WELLS = ("D12", "E12", "A06", "H06")
 
 plate_mapping = {1: 1 / 40, 2: 1 / 160, 3: 1 / 640, 4: 1 / 2560}
