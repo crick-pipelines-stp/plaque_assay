27f47d1293c834eed4c0966f1eac2236e6f878a7
Scott Warchal <scott.warchal@gmail.com>
2021-04-13 16:37:54 +0100

    calculate y-intercept for points evenly spaced on a log-scale (fixes #23)

--- plaque_assay/stats.py
+++ plaque_assay/stats.py
@@ -23,7 +23,7 @@ def find_intersect_on_curve(x_min, x_max, curve, intersect=50):
     In this case one of the curves is just a horizontal line where y = 50.
     TODO: solve this mathematically
     """
-    x = np.linspace(x_min, x_max, 10000)
+    x = np.logspace(np.log10(x_min), np.log10(x_max), 10000)
     line = np.full(x.shape, intersect)
     idx = np.argwhere(np.diff(np.sign(line - curve))).flatten()
     if len(idx) > 1:
@@ -134,7 +134,7 @@ def calc_results_model(name, df, threshold=50, weak_threshold=60):
     x = df["Dilution"].values
     x_min = 0.0000390625
     x_max = 0.25
-    x_interpolated = np.linspace(x_min, x_max, 10000)
+    x_interpolated = np.logspace(np.log10(x_min), np.log10(x_max), 10000)
     y = df["Percentage Infected"].values
     model_params = None
     heuristic = calc_heuristics_dilutions(df, threshold, weak_threshold)
